938d4e23dfde423f8ceffacc4f1b4aa34b79d73b
Emilse Arias <emilsearias@gmail.com>
2023-03-02 17:25:15 -0300

    agregue un modelo de switch

---
+++ js/3-switch.js
@@ -0,0 +1,19 @@
+/*
+switch(opcion){
+    case 1:
+            todas las lineas de codigo que quiero hacer si se cumple que la opcion es la N1
+            break;
+    case '2':
+            todas las lineas de codigo que quiero hacer si se cumple que la opcion es la N2
+            break;
+    case 3:
+            todas las lineas de codigo que quiero hacer si se cumple que la opcion es la N3
+            break;
+    case 'deposito':
+            todas las lineas de codigo que quiero hacer si se cumple que la opcion es la Ndeposito
+            break;
+    default:
+        todas las lineas del caso por defecto
+}
+*/
+
