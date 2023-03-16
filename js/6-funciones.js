a3eb1bc872a74671ccf4a420df20421e671a7d0f
Emilse Arias <emilsearias@gmail.com>
2023-03-16 19:08:37 -0300

    agregar funciones con parametros y sin parametros, tambien funciones  que retornan un valor

---
+++ js/6-funciones.js
@@ -0,0 +1,46 @@
+// aqui creo un par de variables
+
+//funciones declarativas
+function saludar(){
+    // aqui va todo el codigo que sabe hacer saludar
+    document.write('Hola mundo<br>');
+}
+
+function saludoPersonaje(nombre, apellido, alias){
+    // let otroNombre = nombreCompleto(nombre, apellido);
+    document.write(`<p>Hola mi nombre es ${nombreCompleto(nombre, apellido)} me conocen como ${alias}</p>`)
+}
+
+// function nombreCompleto(nombre, apellido){
+//     const nuevoNombre = nombre+', '+apellido;
+//     return nuevoNombre;
+// }
+//funciones expresivas o anonimas
+const nombreCompleto = function (nombre, apellido){
+    return nombre+', '+apellido;
+}
+
+
+// llamar o invocar a la funcion
+
+saludar();
+console.log('prueba');
+saludar();
+
+let nombre= 'Clark';
+let apellido = 'Kent';
+let heroe = 'Superman';
+
+saludoPersonaje('Peter', 'Parker', 'Spiderman');
+
+saludoPersonaje(nombre,apellido, heroe);
+
+nombre = 'Tony'
+apellido = 'Stark'
+heroe = 'Ironamn'
+
+saludoPersonaje(nombre,apellido, heroe);
+
+let otroNombre = nombreCompleto(nombre, apellido);
+console.log(otroNombre);
+
