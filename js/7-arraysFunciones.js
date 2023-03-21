630ee9e3a5b80f4ca1b83ddab9277cb0c93e3506
Emilse Arias <emilsearias@gmail.com>
2023-03-21 18:47:23 -0300

    agregamos un ejemplo de forEach y de map

---
+++ js/7-arraysFunciones.js
@@ -0,0 +1,40 @@
+let productos = [
+    "Hidratante facial",
+    "Tonico exfoliante",
+    "Protector solar factor 50",
+    "Protector solar toque seco factor 50",
+    "Protector solar con color factor 50",
+    "Gel de limpieza facial",
+    "Tonico hidratante",
+    "Sérum hidratante",
+    "Exfoliante de azúcar cherry",
+    "Pads de hidrogel para contorno de ojos",
+    "Mascarilla facial",
+    "Sérum facial con PHA",
+    "Agua micellar",
+    "Mascara de hidratación y reparación",
+    "Protector solar en barra",
+    "Crema contorno de ojos",
+    "Bálsamo labial",
+  ];
+
+  //mostrar la lista de productos
+  const mostrarProductos = (titulo = 'No hay un titulo disponible', arreglo) => {
+    document.write(`<h2>${titulo}</h2>`);
+    document.write("<ul>");
+    // for (let indice = 0; indice < peliculas.length; indice++) {
+    //   document.write(`<li>${peliculas[indice]} </li>`);
+    // }
+    // arreglo.forEach((producto) => { document.write(`<li>${producto} </li>`); })
+    //map
+        arreglo.map(producto => document.write(`<li>${producto} </li>`) )    
+    // forEach, for in, for of
+    document.write("</ul>");
+  };
+
+  mostrarProductos('Lista de productos', productos);
+
+  //filtrar los protectores solares
+
+
+  //buscar un serum
